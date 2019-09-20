3e1d9843187b627769a4e618e0bbc1d07d7d0465
Narek Simonyan <nareksimonyan94@gmail.com>
2019-09-20 15:45:06 +0400

    added gif file

--- Example/Pods/Target Support Files/Pods-NSVLocalizationKit_Example/Pods-NSVLocalizationKit_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-NSVLocalizationKit_Example/Pods-NSVLocalizationKit_Example-frameworks.sh
@@ -161,9 +161,11 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/Localize-Swift/Localize_Swift.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/NSVLocalizationKit/NSVLocalizationKit.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/Localize-Swift/Localize_Swift.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/NSVLocalizationKit/NSVLocalizationKit.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
